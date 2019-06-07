013a7985e47e831bfa40c59bc03ddaa7154e53ad
Gean Lucas <gean.lfpsa@gmail.com>
2019-06-07 09:29:28 -0300

    Criação dos modelos de Tarefas e de usuario

--- TodoList/todos/models.py
+++ TodoList/todos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    name = models.CharField(max_length=50)
+    details = models.TextField()
+    timeStarted = models.DateTimeField(auto_now_add=True)
+    timeFinish = models.DateTimeField()
+    
+    def __str__(self):
+        return self.name
+
+class Owner(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    username = models.CharField(max_length=50)
+    def __str__(self):
+        return self.username
